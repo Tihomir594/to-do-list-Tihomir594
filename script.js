17947125a8212e74219f4c8389255ae63db4a6d6
Tihomir594 <valc0465@student.acitech.org>
2025-04-15 09:16:57 -0400

    Need to make tasks appear when added by making added task buttonn work.

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 //Click on active,completed, and all buttons.Look for items that fall into each category. If all display all items. If active display unchecked items. If if completed display checked items.
 //if checkbox is checked move task to completed and strike through text,decrease text count.//
 //If delete is clicked ,remove task list,decrease task count,Update UI.
-const todoInput = document.getElementById('todoInput')
-const todoCount = document.getElementById('todoCount')
-const  todoList = document.getElementById('todoList')
-const addtodo = document.getElementById('all')
-const todocontainer = document.getElementById('todocontainer')
+const todoInput = document.getElementById('todoInput');
+const todoCount = document.getElementById('todoCount');
+const  todoList = document.getElementById('todoList');
+const addtodo = document.getElementById('all');
+const todocontainer = document.getElementById('todocontainer');
 
-todoInput.addEventListener('click', function(){
-  var paragraph = document.createElement('p');
-  paragraph.innertext = todoInput.value;
-  todocontainer.appendChild(paragraph);
-})
-
+//Creates tasks//
+    function todoList() {
+    var item = document.getElementById("todoInput").value;
+    var text = document.createTextNode(item);
+    var newItem = document.createElement("li");
+    newItem.appendChild(text);
+    document.getEementById("todoList").appendChild(newItem);
+  }
